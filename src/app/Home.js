c3402fbcfb62ec3c181277546ff44f0de739431f
Eli Pleaner <eliipleaner@gmail.com>
2023-10-22 13:56:08 -0700

    add mobile home

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,30 +1,39 @@
 import Bubbles from '@/app/Bubbles';
 import Section from '@/app/Section';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <div className='absolute'>
+      <div className='absolute max-sm:hidden'>
         <Bubbles />
       </div>
       <Section>
-        <div className='text-5xl font-semibold text-cyan'>
+        <div className='max-sm:text-[36px] text-5xl font-semibold text-cyan'>
           Delightful and memorable magic
         </div>
-        <div className='text-center text-[136px] text-orange font-bold'>
+        <div className='text-center max-sm:text-7xl text-[136px] text-orange font-bold'>
           Magic Al
         </div>
-        <div className='text-5xl font-semibold text-cyan text-right'>
+        <div className='max-sm:pl-[100px] max-sm:text-[36px] text-5xl font-semibold text-cyan text-right'>
           for children of all ages
         </div>
       </Section>
-      <div className='absolute right-[180px] bottom-[180px] text-right pr-[60px] flex items-end justify-end z-20'>
-        <Link href='/contact'>
-          <button className='font-sans flex items-center justify-center bg-blue rounded-2xl text-cream uppercase font-bold text-[32px] py-2 px-3 hover:bg-transparent hover:text-blue hover:border-blue border-2 border-transparent transition-colors'>
-            Book now
-          </button>
-        </Link>
+      <div className='absolute bottom-0'>
+        <Image
+          src='/bunny.png'
+          width={480}
+          height={354}
+          alt='Bunny wearing purple overalls eating a carrot'
+        />
+        <div className='w-full absolute top-[-30px] max-sm:pr-[30px] pr-[60px] flex items-end justify-end z-20'>
+          <Link href='/contact'>
+            <button className='font-sans flex items-center justify-center max-sm:bg-cyan bg-blue rounded-2xl max-sm:text-blue text-cream uppercase font-bold max-sm:text-[24px] text-[32px] py-2 px-3 hover:bg-transparent hover:text-blue hover:border-blue border-2 border-transparent transition-colors'>
+              Book now
+            </button>
+          </Link>
+        </div>
       </div>
     </>
   );
--- src/app/Section.js
+++ src/app/Section.js
@@ -1,6 +1,6 @@
 export default function Section({ children }) {
   return (
-    <section className='z-10 px-[180px] pt-[300px] pb-[420px] font-sans w-full min-h-screen flex flex-col justify-center'>
+    <section className='max-sm:gap-[30px] z-10 max-sm:px-[30px] px-[180px] max-sm:pt-[120px] pt-[300px] pb-[420px] font-sans w-full min-h-screen flex flex-col justify-center'>
       {children}
     </section>
   );
