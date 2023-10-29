31a1a973d0b603149ec391e54a39dbbbbd0b4cb1
Eli Pleaner <eliipleaner@gmail.com>
2023-10-29 11:50:12 -0700

    fix itemvariants

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -5,7 +5,7 @@ import { useState, useCallback, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import usePageTransition from '../hooks/usePageTransition.js';
 import { motion } from 'framer-motion';
-import { itemVariant } from '@/app/Animate';
+import { itemVariants } from '@/app/Animate';
 
 export default function Footer({ href, label, bgColor }) {
   const { go, setGo, transitionComponent } = usePageTransition({
@@ -24,7 +24,7 @@ export default function Footer({ href, label, bgColor }) {
           alt='Bunny wearing purple overalls eating a carrot'
         />
         <motion.div
-          variant={itemVariant}
+          variant={itemVariants}
           className='w-full absolute max-sm:top-[-30px] top-[-60px] max-sm:pr-[30px] pr-[60px] flex items-end justify-end z-20'>
           <button
             className='font-sans flex items-center justify-center max-sm:bg-cyan bg-blue rounded-2xl max-sm:text-blue text-cream uppercase font-bold max-sm:text-[24px] text-[32px] py-2 px-3 hover:bg-cream hover:text-blue max-sm:hover:border-cyan hover:border-blue border-2 border-transparent transition-colors'
